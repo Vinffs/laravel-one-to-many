0503299c139514cb6406a1c4aa191b93ef1dd627
Vinffs <vin.demiranda@gmail.com>
2024-01-16 14:50:02 +0100

    added in types.show all projects linked to that type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -4,9 +4,11 @@
         <h1>
             {{ $type->name }}
         </h1>
-        @foreach ($type->projects as $project)
-            <h2>{{ $project->title }}</h2>
-        @endforeach
+        @forelse ($type->projects as $project)
+            <li>{{ $project->title }}</li>
+        @empty
+            <li>No projects</li>
+        @endforelse
         <a href="{{ route('admin.types.index') }}" class="btn btn-danger">Return</a>
     </section>
 @endsection
