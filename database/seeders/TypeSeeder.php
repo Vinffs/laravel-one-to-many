acd358cdab692c3860f04fb7999aa3ecc30ef86d
Vinffs <vin.demiranda@gmail.com>
2024-01-16 12:13:15 +0100

    Seeded table types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,12 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['Frontend', 'Backend', 'Fullstack'];
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($type, '-');
+            $newType->save();
+        }
     }
 }
