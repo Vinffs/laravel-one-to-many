8ee0bf062452cdd79be5df0caf3b6251c56a2728
Vinffs <vin.demiranda@gmail.com>
2024-01-16 14:53:42 +0100

    added to projects.show the type linked to the project

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,7 +6,7 @@
         </h1>
         <div><img src="{{ asset("storage/$project->thumb") }}" alt="{{ $project->title }}"></div>
         <p>{{ $project->description }}</p>
-
+        <div>{{ $project->type ? $project->type->name : 'Uncategorized' }}</div>
         <a href="{{ route('admin.projects.index') }}" class="btn btn-danger">Return</a>
     </section>
 @endsection
